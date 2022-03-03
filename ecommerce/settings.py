04710c5733e70b93fcb48514dec7c86f35c5ea28
dalbeertws <dalbeer.tws@tekkiwebsolutions.com>
2022-03-03 18:54:28 +0530

    Implemented Social Login i.e Google

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -149,7 +149,7 @@ MEDIA_ROOT=BASE_DIR/'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-SITE_ID = 1
+SITE_ID = 2
 
 # Provider specific settings
 SOCIALACCOUNT_PROVIDERS = {
--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home,name="home"),
-    path('login/', views.signin,name="login"),
-    path('register/', views.signup,name="signup"),
-    path('logout/', views.signout,name="logout"),
-]
